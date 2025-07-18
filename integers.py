e69d5e85b18d8b174f12a05943bcd500db684f68
Daler Mirzoev <dalee2k@gmail.com>
2025-07-18 15:11:16 +0300

    created BMI measaure app

---
+++ integers.py
@@ -0,0 +1,7 @@
+x, y, z = 2, 4, 6 #several assignments
+print(x+z)
+print(y-z)
+print(x*x)
+print(x**3) #exponent
+print(z/x)
+print(z//x) #division with the result as integer
---
+++ methods.py
@@ -0,0 +1,6 @@
+name = 'mirzoev daler'
+realname = name.removeprefix('mirzoev') #you can also remove suffix
+name = realname.strip()
+print(name)
+#to meausure length
+print(len(name))
