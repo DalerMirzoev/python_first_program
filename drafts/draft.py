63d88b3177004d14334b44259aa526d38cf73eb1
Daler Mirzoev <dalee2k@gmail.com>
2025-07-19 21:26:50 +0300

    some changes

--- drafts/draft.py
+++
@@ -1,4 +0,0 @@
-cars=['volkswagen', 'audi', 'BMW', 'mercedes-benz', 'rolls-royce']
-print(f'My first three cars are: {cars[:3]}.')
-print(f'My middle three cars are: {cars[1:4]}.')
-print(f'My last three cars are: {cars[-3:]}.')
